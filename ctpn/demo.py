e884ca2ca7b3f5d4ffe81a3fcc99a30fd2250bb5
alorozco53 <alorozco.patriot53@gmail.com>
2018-06-13 15:36:26 -0500

    [ADDED] a box point generator function

--- ctpn/demo.py
+++ ctpn/demo.py
@@ -6,7 +6,7 @@ import glob
 import shutil
 sys.path.append(os.getcwd())
 from lib.networks.factory import get_network
-from lib.fast_rcnn.config import cfg,cfg_from_file
+from lib.fast_rcnn.config import cfg, cfg_from_file
 from lib.fast_rcnn.test import test_ctpn
 from lib.utils.timer import Timer
 from lib.text_connector.detectors import TextDetector
@@ -19,6 +19,16 @@ def resize_im(im, scale, max_scale=None):
         f=float(max_scale)/max(im.shape[0], im.shape[1])
     return cv2.resize(im, None,None, fx=f, fy=f,interpolation=cv2.INTER_LINEAR), f
 
+def box_point_generator(img, boxes, scale):
+    for box in boxes:
+        if np.linalg.norm(box[0] - box[1]) < 5 or np.linalg.norm(box[3] - box[0]) < 5:
+            continue
+        else:
+            min_x = min(int(box[0]/scale), int(box[2]/scale), int(box[4]/scale), int(box[6]/scale))
+            min_y = min(int(box[1]/scale), int(box[3]/scale), int(box[5]/scale), int(box[7]/scale))
+            max_x = max(int(box[0]/scale), int(box[2]/scale), int(box[4]/scale), int(box[6]/scale))
+            max_y = max(int(box[1]/scale), int(box[3]/scale), int(box[5]/scale), int(box[7]/scale))
+            yield [min_x, min_y, max_x, max_y]
 
 def draw_boxes(img,image_name,boxes,scale):
     base_name = image_name.split('/')[-1]
@@ -46,7 +56,7 @@ def draw_boxes(img,image_name,boxes,scale):
     img=cv2.resize(img, None, None, fx=1.0/scale, fy=1.0/scale, interpolation=cv2.INTER_LINEAR)
     cv2.imwrite(os.path.join("data/results", base_name), img)
 
-def ctpn(sess, net, image_name):
+def ctpn(sess, net, image_name, serialize=True):
     timer = Timer()
     timer.tic()
 
@@ -56,10 +66,13 @@ def ctpn(sess, net, image_name):
 
     textdetector = TextDetector()
     boxes = textdetector.detect(boxes, scores[:, np.newaxis], img.shape[:2])
-    draw_boxes(img, image_name, boxes, scale)
     timer.toc()
     print(('Detection took {:.3f}s for '
            '{:d} object proposals').format(timer.total_time, boxes.shape[0]))
+    if serialize:
+        draw_boxes(img, image_name, boxes, scale)
+    else:
+        return list(box_point_generator(img, boxes, scale))
 
 
 
@@ -98,4 +111,3 @@ if __name__ == '__main__':
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
         print(('Demo for {:s}'.format(im_name)))
         ctpn(sess, net, im_name)
-
